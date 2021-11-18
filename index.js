a06a5c2a0b9b244b48cd0986c0a1511dbba33cb6
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-18 19:48:55 +0100

    + operator working as expected

--- index.js
+++ index.js
@@ -6,10 +6,26 @@
  * - equals: returns the value (thanks to an 'eval' function)
  * - clear: clears the history
  */
+class Calculator {
+  constructor() {}
+  history() {
+    // Once key is pressed, it keeps the last value in
+  }
+  equals() {
+    // Returns the value thanks to an eval function
+  }
+  clear() {
+    // Clears the history
+  }
+}
 
 const calculatorScreen = document.querySelector("#calculator .screen");
 const equals = document.querySelector("#calculator .eval");
-
+let lastValue;
+let storedValue;
+let valuesArray = [];
+const operators = ["=", "+", "x", "/", "-"];
+let operatorPressed = false;
 /**
  * This function below write the value of the pressed key on the screen
  * The += is the equivalent of:
@@ -17,12 +33,18 @@ const equals = document.querySelector("#calculator .eval");
  *
  **/
 function print(val) {
+  if (operatorPressed) {
+    calculatorScreen.innerHTML = "";
+    operatorPressed = false;
+  }
+
   calculatorScreen.innerHTML += val;
+  valuesArray.push(val);
 }
 
 //this code listen to every key on the calculator and add the value on the screen
 document.querySelectorAll("#calculator span").forEach((key) => {
-  if (key.innerText !== "=") {
+  if (!operators.includes(key.innerText)) {
     key.addEventListener("click", (e) => print(e.target.innerText));
   }
 });
@@ -31,4 +53,25 @@ document
   .querySelector("#calculator .clear")
   .addEventListener("click", () => (calculatorScreen.innerHTML = ""));
 
+document.querySelector(".plus").addEventListener("click", () => {
+  if (!storedValue) {
+    storedValue = Number(valuesArray.join(""));
+    valuesArray = [];
+  } else {
+    calculatorScreen.innerHTML = "";
+    lastValue = Number(valuesArray.join(""));
+    storedValue += lastValue;
+    print(storedValue);
+    lastValue = 0;
+    valuesArray = [];
+  }
+  operatorPressed = true;
+});
 // Implement here the event when the = key is pressed
+document.querySelector(".keys .eval").addEventListener("click", () => {
+  const stringValue = lastValue.join("");
+  const numValue = Number(stringValue);
+  console.log(numValue);
+  console.log(stringValue);
+  lastValue = [];
+});
